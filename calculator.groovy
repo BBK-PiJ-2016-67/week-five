1a0462ff948ca86cc3a26d77e3cd29e9c73d4df5
Mr Lucas Gordon <lgordo04@dcs.bbk.ac.uk>
2016-10-31 18:46:41 +0000

    Adds calculator exercise.

---
+++ calculator.groovy
@@ -0,0 +1,21 @@
+Calculator calculator = new Calculator();
+
+int result = calculator.add(10, 2)
+
+println("10 plus 2 is " + result)
+
+result = calculator.subtract(10, 2)
+
+println("10 minus 2 is " + result)
+
+result = calculator.multiply(10, 2)
+
+println("10 times 2 is " + result)
+
+result = calculator.divide(10, 2)
+
+println("10 divided by 2 is " + result)
+
+result = calculator.modulus(10, 2)
+
+println("10 modulus 2 is " + result)
